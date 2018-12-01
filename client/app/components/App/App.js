79ad5ee7d8208fd0de3e94d0491f280b887dd904
jeffkealy <jeffkealy@yahoo.com>
2018-12-01 16:37:57 -0600

    propTypes

--- client/app/components/App/App.js
+++ client/app/components/App/App.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
-
-import Header from '../Header/Header';
-
-const App = ({ children }) => (
-  <>
-    <Header />
-
-    <main>
-      {children}
-    </main>
-
-
-  </>
-);
-
-export default App;
-
-// import React, { Component } from 'react'
+// import React, { Component } from 'react';
+//
 // import Header from '../Header/Header';
-// import PropTypes from 'prop-types';
 //
-// class App extends Component {
-//   static propTypes = {
-//     children: PropTypes.node
-//   }
-//
-//
-// render() {
-//   const { children } = this.props
-//   console.log(PropTypes);
-//
-//   return (
-//     <>
+// const App = ({ children }) => (
+//   <>
 //     <Header />
+//
 //     <main>
 //       {children}
 //     </main>
-//     </>
-//   )
-// }
-// }
 //
-// export default App
+//
+//   </>
+// );
+//
+// export default App;
+
+import React, { Component } from 'react'
+import Header from '../Header/Header';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  }
+
+
+render() {
+  const { children } = this.props
+
+  return (
+    <>
+    <Header />
+    <main>
+      {children}
+    </main>
+    </>
+  )
+}
+}
+
+export default App
